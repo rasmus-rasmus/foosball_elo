0b909b31042753056172c97a8cd2b1ea432fbec6
Rasmus Stavenuiter <rkst@rd8.tech>
2023-10-06 08:23:13 +0200

    Implement and add templates for index, all players, and submit form views

--- foosball_elo/elo/urls.py
+++ foosball_elo/elo/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name='elo_app'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('all/', views.AllView.as_view(), name='all'),
     path('<int:player_id>/', views.player_detail, name='player_detail'),
+    path('game/submit_form', views.SubmitFormView.as_view(), name='submit_form'),
     path('game/submit', views.submit_game, name='submit_game')
 ]
--- foosball_elo/elo/views.py
+++ foosball_elo/elo/views.py
@@ -1,13 +1,55 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from django.views import generic
+from .models import Player, Game
 
 # Create your views here.
-def index(request: HttpRequest):
-    #TODO: Implement
-    return HttpResponse("<h2>Welcome to the foosball elo rating app!</h2>")
+class IndexView(generic.ListView):
+    template_name = 'elo/index.html'
+    context_object_name = 'top_5_list'
+    
+    def get_queryset(self) -> QuerySet[Player]:
+        return Player.objects.order_by('-elo_rating')[:5]
+    
+class AllView(generic.ListView):
+    model = Player
+    
+    def get_queryset(self) -> QuerySet[Player]:
+        return Player.objects.order_by('-elo_rating')
+    
+class SubmitFormView(generic.ListView):
+    template_name = 'elo/submit_form.html'
+    context_object_name = 'all_players_list'
+    
+    def get_queryset(self):
+        return Player.objects.order_by('player_name')
+    
 
 def submit_game(request: HttpRequest):
-    #TODO: Implement
+    data = request.POST
+    try:
+        team_1_defense = Player.objects.get(pk=data['team_1_defense'])
+        team_1_attack = Player.objects.get(pk=data['team_1_attack'])
+        team_2_defense = Player.objects.get(pk=data['team_2_defense'])
+        team_2_attack = Player.objects.get(pk=data['team_2_attack'])
+        team_1_score = data['team_1_score']
+        team_2_score = data['team_2_score']
+        date = data['date']
+    except (KeyError, Player.DoesNotExist):
+        return render(request, 'elo/submit_form.html', {
+            'all_players_list': Player.objects.order_by('player_name'),
+            'error_message': 'Please fill out all game data'
+        })
+    Game.objects.create(team_1_defense=team_1_defense,
+                        team_1_attack=team_1_attack,
+                        team_2_defense=team_2_defense,
+                        team_2_attack=team_2_attack,
+                        team_1_score=team_1_score,
+                        team_2_score=team_2_score,
+                        date_played=date)
+    #TODO: Implement updating player statistics
     return HttpResponse("<h2>You just submitted a game</h2>")
 
 def player_detail(request: HttpRequest, player_id: int):
